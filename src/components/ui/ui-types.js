40c1b06d358460a0db286b813503aec4cc2426ea
Roman <romashym84@gmail.com>
2025-01-23 16:11:24 +0200

    add dropdown components

---
+++ src/components/ui/ui-types.js
@@ -0,0 +1,14 @@
+export const UI_SIZE = {
+  DEFAULT: 'default',
+  SMALL: 'small',
+  LARGE: 'large',
+}
+export const KEY_CODE = {
+  ESCAPE: 'Escape',
+  SPACE: 'Space',
+  ENTER: 'Enter',
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  LEFT: 'ArrowLeft',
+  RIGHT: 'ArrowRight',
+}
